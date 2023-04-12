5815d832b4b57e056de7aed2bcb99a07892f36ef
francis kinyuru <franciskinyuru26@gmail.com>
2023-04-12 18:59:18 +0300

    added dockerfiles to client and backend folder and a docker compose file in the root folder

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const multer = require('multer');
 const upload = multer();
+require('dotenv').config();
 
 const productRoute = require('./routes/api/productRoute');
 
@@ -11,7 +12,7 @@ let mongodb_url = 'mongodb://localhost/';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
-const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
+const MONGODB_URI = process.env.MONGODB_URI
 mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true  } )
 let db = mongoose.connection;
 
@@ -41,7 +42,7 @@ app.use(cors());
 app.use('/api/products', productRoute)
 
 // Define the PORT
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5050
 
 app.listen(PORT, ()=>{
     console.log(`Server listening on port ${PORT}`)
